5674294a069307b301ba33b187e2166fe56de42d
Matthew Carlson <carlson.matt.r@gmail.com>
2022-04-07 17:54:22 -0700

    asking for help

--- script.js
+++ script.js
@@ -3,32 +3,33 @@ $("#currentDay").text(currentDay.format("MMMM Do, YYYY"));
 
 
 var hourMoment = currentDay.format("H");
+console.log(hourMoment);
 
-var timeBlock = $(".time-block");
 
+var containerAtt = $(".container");
+timeBlock = $(".time-block");
+var hourID = containerAtt.children("div").attr("id");
+parsedHour = parseInt(hourID - 2))
 
-var hourID = timeBlock.attr("#hour-");
-console.log(hourID);
+for (var i = 9; i < containerAtt.length; i++) 
 
-function timeCheck(){
-    for (var i = 9; i < timeBlock.length; i++){
-    
-    if (parseInt(hourID.slice(-2) < hourMoment)){
-        timeBlock.addClass("past");
-    }
-    else if (parseInt(hourID.slice(-2) === hourMoment)){
-        timeBlock.addClass("present");
-    }
-    else{
-        timeBlock.addClass("future");
-    }
+if (hourID[i] < hourMoment){
+    timeBlock[i].addClass(".past");
+    timeBlock[i].removeClass(".present");
+    timeBlock[i].removeClass(".future");
 }
+else if (hourID[i] === hourMoment){
+    timeBlock[i].addClass(".present");
+    timeBlock[i].removeClass(".past");
+    timeBlock[i].removeClass(".future");
 }
-timeCheck();
+else {
+    timeBlock[i].addClass(".future");
+    timeBlock[i].removeClass(".present");
+    timeBlock[i].removeClass(".past");
+}
 
 
- 
-// var hourMoment = computer gets hour from Moment
 //           for timeBlock in all divs with class time-block
 //             var hourID = getting ID from timeBlock 
 //             if hourMoment is equal to hourID 
@@ -37,9 +38,36 @@ timeCheck();
 //               add past class to timeBlock
 //             else 
 //               add future class to timeBlock
+//get the number from the ID
+//parse it into a string
+
+// var timeBlock = $(".time-block");
+// var hourLoop = $(".time-block").attr("id".value);
+// var hourID = parseInt(slice(hourLoop - 2));
+// console.log(hourID);
+
+// timeBlock.each (function timeCheck(el){
+    
+
+//     for (var i = 9; i < timeBlock.length; i++)
+
+//     if (hourID[i] < hourMoment) {
+//         timeBlock[i].addClass("past")
+//     }
+//     else if (hourID[i] === hourMoment) {
+//         timeBlock[i].addClass("present");
+//     }
+//     else {
+//         timeBlock[i].addClass("future");
+//     }
+// })
+
+// timeCheck(el);
+
+ 
+// var hourMoment = computer gets hour from Moment
+
 
 
 //take the hour part of IDs out
 //use parseint to turn from a string into a sequence
-
-
